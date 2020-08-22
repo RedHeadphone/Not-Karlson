2469f9b8f73c4c12d2084923e7f8fa2f89aa6287
RedHeadphone <khilawala92@gmail.com>
2020-08-22 18:01:55 +0530

    ok

--- parkour/script/bullet.gd
+++ parkour/script/bullet.gd
@@ -2,15 +2,6 @@ extends RigidBody
 
 
 func _ready():
-#	var t=get_global_transform()
-#	var tt=self
-#	var tyt=get_parent().get_parent().get_parent()
-#	get_parent().remove_child(self)
-#	print(tyt.world)
-#	tyt.world.add_child(tt)
-#	self.set_global_transform(t)
-	#set_as_toplevel(true)
-	#self.scale=Vector3(1,1,1)
 	pass
 
 var del=false
@@ -23,6 +14,7 @@ func _process(delta):
 		time+=delta
 	if time>0.7:
 		queue_free()
+		$CollisionShape.disabled=true
 
 
 func _on_Area_body_entered(body):
@@ -30,6 +22,5 @@ func _on_Area_body_entered(body):
 	$MeshInstance.visible=false
 	$Position3D/Trail3D.visible=false
 	$Particles.emitting=true
-	$CollisionShape.disabled=true
 	mode=RigidBody.MODE_STATIC
 
